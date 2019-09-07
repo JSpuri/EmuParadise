a9b9bbac0161f270845cbca15845c9bc7e8dbd45
Spuri <spurijoao@gmail.com>
2019-09-06 21:23:28 -0300

    imprime bytes

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>       // std::cout, std::endl
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
+#include <fstream>
+using namespace std;
+
 
 struct Instrucao {
   int opcode;
@@ -19,29 +22,43 @@ void log(){
 }
 
 void LeCartucho(const char *arquivo){
+	ifstream binario;
+	binario.open(arquivo, ios::in | ios::binary | ios::ate);
+	
 	int pc = 0;
-    while(1){
+	streampos size;
+	char * memblock;
+	if(binario.is_open()){
+		size = binario.tellg();
+		memblock = new char [size];
+		binario.seekg(0, ios::beg);
+		binario.read(memblock, size);
+		binario.close();
+	}
+	
+    for(pc = 0; pc < size; pc++){
         Instrucao instrucao;
 
 		instrucao = Decodifica(pc);
 		
+		for(int i = 0; i < 8; i++) {
+            printf("%d", !!((memblock[pc] << i) & 0x80));
+        }
+        printf("\n");
+		
 		switch(instrucao.opcode){
-			case 0:
-			    break;
-		    case 1:
-			    break;
 			default:
 			    printf("erro\n");
 		}
 		log();
-		std::this_thread::sleep_for (std::chrono::seconds(1));
+		//std::this_thread::sleep_for (std::chrono::seconds(1));
     }
 }
 
 int main(int argc, const char *argv[]){
-    const char *arquivo = argv[1];
+    const char *nomearquivo = argv[1];
 
-    LeCartucho(arquivo);
+    LeCartucho(nomearquivo);
 
     return 0;
 }
