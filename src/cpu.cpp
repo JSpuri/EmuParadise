840d9478c80982c33a8a99c4fae145e0c7a7adc4
weialex <plank@plancton.localdomain>
2019-09-27 17:48:28 -0300

    carry for adc, sbc

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -28,16 +28,16 @@ CPU::CPU(uint16_t reset_addr) {
     this->a = 0;
     this->x = 0;
     this->y = 0;
-    
+
     this->sp = 0xFA;
     this->pc = reset_addr;
-    
+
     this->ps[C] = 0;
     this->ps[Z] = 0;
     this->ps[I] = 1;
     this->ps[D] = 0;
     this->ps[B] = 1;
-    this->ps[B+1] = 1;
+    this->ps[B-1] = 1;
     this->ps[V] = 0;
     this->ps[N] = 0;
 }
