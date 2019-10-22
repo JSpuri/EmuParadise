51b80ea8206e0a6c65f0258f0707dc027f619b23
Eduardo 'mrtheduts' Souza <eduardomoreirafds@gmail.com>
2019-10-22 03:44:11 -0300

    mend

--- src/headers/processor.hpp
+++ src/headers/processor.hpp
@@ -5,16 +5,12 @@
 #include <cstdint>
 #include <vector>
 
-class AddressBus;
-
 class Processor {
 
     public:
         virtual void WriteTo(uint16_t addr, int8_t value) =0;
         virtual uint8_t ReadFrom(uint16_t addr) =0;
         virtual ~Processor() {};
-    protected:
-        AddressBus *addr_bus;
 };
 
 #endif
